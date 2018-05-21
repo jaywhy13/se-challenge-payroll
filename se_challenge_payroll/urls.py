9d5e7e8b7b0bad2826b6bfa89a131419898da776
Jean-Mark Wright <jeanmark.wright@gmail.com>
2018-05-21 17:15:43 -0500

    Added API views and the home view

--- se_challenge_payroll/urls.py
+++ se_challenge_payroll/urls.py
@@ -13,9 +13,19 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
 
+from rest_framework.routers import DefaultRouter
+
+from payroll.views import HomeView, PartnerViewSet
+
+router = DefaultRouter()
+router.register(r'partner', PartnerViewSet)
+
 urlpatterns = [
+    url(r'^$', HomeView.as_view(), name='home'),
     url(r'^admin/', admin.site.urls),
+    # API
+    url(r'^api/', include(router.urls)),
 ]
