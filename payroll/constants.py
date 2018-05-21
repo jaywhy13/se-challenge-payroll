4ca914dc2309abf5f3e68f62a35e171e0bb636e8
Jean-Mark Wright <jeanmark.wright@gmail.com>
2018-05-21 17:14:48 -0500

    Added constants

---
+++ payroll/constants.py
@@ -0,0 +1,22 @@
+DATE_FORMATS = (
+    ("dd/mm/yyyy", "%d/%m/%Y"),
+    ("mm/dd/yyyy", "%m/%d/%Y"),
+)
+
+COUNTRIES = (
+    ("Jamaica", "JA"),
+    ("Canada", "CA"),
+    ("USA", "USA"),
+)
+
+PAY_PERIODS = (
+    ("Bi-weekly", "bi-weekly"),
+    ("Monthly", "monthly"),
+)
+
+TIMESHEET_UNITS = (
+    ("Hour", "hour"),
+    ("Day", "day"),
+    ("Week", "week"),
+    ("Month", "month"),
+)
