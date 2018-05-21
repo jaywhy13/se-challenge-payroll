fec29209ce3865f66bdb836edbe864a3d26d13e8
Jean-Mark Wright <jeanmark.wright@gmail.com>
2018-05-21 17:15:19 -0500

    Added basic views

--- payroll/views.py
+++ payroll/views.py
@@ -1,2 +1,21 @@
+from django.views.generic.base import TemplateView
 
-# Create your views here.
+from rest_framework import viewsets
+
+from payroll.models import Partner
+
+from payroll.serializers import (
+    PartnerSerializer, PartnerProfileSerializer,
+    EmployeeGroupSerializer, EmployeeTimeSerializer
+)
+
+
+class HomeView(TemplateView):
+
+    template_name = "index.html"
+
+
+class PartnerViewSet(viewsets.ModelViewSet):
+
+    queryset = Partner.objects.all()
+    serializer_class = PartnerSerializer
