9aeb6b8ba1445c5a1e84ed49148e60b4c8c149c2
Jean-Mark Wright <jeanmark.wright@gmail.com>
2018-05-30 22:31:19 -0500

    Setup admin so we can manage the different models

--- payroll/admin.py
+++ payroll/admin.py
@@ -1,2 +1,32 @@
 from django.contrib import admin
 
+from payroll.models import (
+    Partner, PartnerProfile, EmployeeGroup, EmployeeTime,
+    EmployeeTimesheet
+)
+
+admin.site.register(Partner)
+admin.site.register(PartnerProfile)
+admin.site.register(EmployeeGroup)
+admin.site.register(EmployeeTimesheet)
+
+
+class EmployeeTimeAdmin(admin.ModelAdmin):
+
+    list_display = [
+        'employee_id', 'start_date', 'end_date',
+        'unit', 'quantity', 'timesheet', 'employee_group',
+        'rate', 'amount_paid']
+
+    list_filter = [
+        'employee_id', 'employee_group', 'timesheet',
+        'timesheet__partner_profile',
+    ]
+
+    def rate(self, obj):
+        return obj.employee_group.rate
+
+    def amount_paid(self, obj):
+        return obj.employee_group.rate * obj.quantity
+
+admin.site.register(EmployeeTime, EmployeeTimeAdmin)
