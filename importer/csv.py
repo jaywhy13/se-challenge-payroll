04346dbdf8d5c688695452d972e40d33939264a1
Jean-Mark Wright <jeanmark.wright@gmail.com>
2018-06-06 18:54:38 -0500

    This is way easier when you use the DictReader

--- importer/csv.py
+++ importer/csv.py
@@ -11,8 +11,6 @@ class CSVImporter(Importer):
         super().__init__(**kwargs)
 
     def get_data(self):
-        csvreader = csv.reader(self.file)
+        csvreader = csv.DictReader(self.file)
         for row in csvreader:
-            field_names = ["date", "hours_worked", "employee_id", "job_group"]
-            row_dict = dict(zip(field_names, row))
-            yield row_dict
+            yield row
