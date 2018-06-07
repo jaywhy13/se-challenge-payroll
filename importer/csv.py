162fd595f7048c62eb18106470c38f62cce57407
Jean-Mark Wright <jeanmark.wright@gmail.com>
2018-06-06 19:15:56 -0500

    Remove spaces from field names

--- importer/csv.py
+++ importer/csv.py
@@ -13,4 +13,17 @@ class CSVImporter(Importer):
     def get_data(self):
         csvreader = csv.DictReader(self.file)
         for row in csvreader:
+            row = self.sanitize_field_names(row)
+            print("Yielding row: {}".format(row))
             yield row
+
+    def sanitize_field_names(self, row):
+        """ Removes spaces from field names
+        """
+        sanitized_row = {}
+        for field_name in row.keys():
+            val = row[field_name]
+            if " " in field_name:
+                field_name = field_name.replace(" ", "_")
+            sanitized_row[field_name] = val
+        return sanitized_row
